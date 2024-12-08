64d0c76d3485a29ee3e6d4ed1824665e5a3f9c3d
tiemimaeda <ptiemi.maeda@gmail.com>
2024-12-08 17:11:55 -0300

    alteracoes aula 10 pratica integradora 4sem

---
+++ modulo10/template-materialui/src/components/header.js
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import { purple } from '@mui/material/colors';
+//import MenuIcon from '@mui/icons-material/Menu';
+ 
+function Header() {
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              {/*  
+              <MenuIcon />
+              */}
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} color={purple}>
+              Gerenciamento de Projetos - Alteração Pensar e Responder - Módulo 10
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    );
+}
+ 
+export default Header;
